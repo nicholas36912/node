d94b73464bb4f2c14de72e20fcb2f20440a13898
Nicholas Scinocco <nicholas.scinocco2@gmail.com>
2023-09-12 08:56:57 -0400

    Initial commit

--- fileOperations.js
+++ fileOperations.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
- 
+
 // Read a file
 fs.readFile('example.txt', 'utf8', (err, data) => {
     if (err) {
@@ -9,4 +9,23 @@ fs.readFile('example.txt', 'utf8', (err, data) => {
     console.log('File content:', data);
 });
 
-readFile(filePath);
+// Write content to a file
+const contentToWrite = 'Hello, this is some content to write to the file.';
+fs.writeFile('output.txt', contentToWrite, (err) => {
+    if (err) {
+        console.error('Error writing to the file:', err);
+        return;
+    }
+    console.log('File has been written successfully.');
+});
+
+// Delete a file
+fs.unlink('fileToDelete.txt', (err) => {
+    if (err) {
+        console.error('Error deleting the file:', err);
+        return;
+    }
+    console.log('File has been deleted successfully.');
+});
+
+
